9f119c1f7a5766e283f9bb795fef4a771087478c
bstrsanya <baas157@bk.ru>
2024-08-27 16:37:56 +0300

    Version #4
    Added print bin massive

--- massive.cpp
+++ massive.cpp
@@ -10,19 +10,26 @@ void PrintfOneMossive (int* data, size_t size);
 
 void EachPlusConst (int* data_2);
 
+void PrintTwoMossive (int data[][6], size_t nStolb, size_t nStrock);
+
 int main()
 {
     int data[6] = {10, 20, 30, 40, 50, 60};
 
     int data_2[6] = {100, 200, 300, 400, 500, -1};
 
-    ShiftOneElementLeft (data, sizeof (data) / sizeof (data[0]));
-    PrintfOneMossive (data, sizeof (data) / sizeof (data[0]));
+    int data_bin[][6] = {{10, 11, 12, 13, 14, 15},
+                         {20, 21, 22, 23, 24, 25},
+                         {30, 31, 32, 33, 34, 35},
+                         {40, 41, 42, 43, 44, 45}};
+    
+    PrintTwoMossive (data_bin, sizeof (data_bin[0]) / sizeof (data_bin[0][0]), sizeof (data_bin) / sizeof (data_bin[0]));
 
-    EachPlusConst (data_2);
-    PrintfOneMossive (data_2, sizeof (data_2) / sizeof (data_2[0]));
+    //ShiftOneElementLeft (data, sizeof (data) / sizeof (data[0]));
+    //PrintfOneMossive (data, sizeof (data) / sizeof (data[0]));
 
-
+    //EachPlusConst (data_2);
+    //PrintfOneMossive (data_2, sizeof (data_2) / sizeof (data_2[0]));
 }
 
 void ShiftOneElementLeft (int* data, size_t size)
@@ -54,4 +61,14 @@ void EachPlusConst (int* data_2)
         data_2[i] += dob;
         i++;
     }
+}
+
+void PrintTwoMossive (int data[][6], size_t nStolb, size_t nStrock)
+{
+    for (size_t y = 0; y < nStrock; y++)
+        {
+        for (size_t x = 0; x < nStolb; x++)
+            printf("%d ", data[y][x]);
+        printf ("\n");
+        }
 }
